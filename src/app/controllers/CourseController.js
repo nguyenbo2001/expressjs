83a0a98f73b90c3a23f59e1483a82d5085da5cd9
Bo Nguyen <nguyenbo2001@gmail.com>
2020-11-01 22:58:23 +0700

    detail courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -14,6 +14,19 @@ class CourseController {
             })
             .catch(next);
     }
+
+    create(req, res, next) {
+        res.render('create');
+    }
+
+    store(req, res, next) {
+        const courseData = req.body;
+        courseData.image = `https://img.youtube.com/vi/${req.body.videoID}/sddefault.jpg`;
+
+        Course.create(courseData);
+
+        res.redirect('/');
+    }
 }
 
 module.exports = new CourseController;
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const mongooseSlug = require('mongoose-slug-generator');
 const Schema = mongoose.Schema;
 
+mongoose.plugin(mongooseSlug);
+
 const Course = new Schema({
-    name: { type: String, maxlength: 255 },
+    name: { type: String, maxlength: 255, required: true },
     description: { type: String, maxlength: 255 },
     image: { type: String, maxlength: 255 },
-    createdAt: { type: Date, default: Date.now() },
-    updatedAt: { type: Date, default: Date.now() },
+    videoID: { type: String, required: true },
+    level: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+}, {
+    timestamps: true
 });
 
 
