625bc6ce26e51322630d7e65fb14b1939b1b5eb6
Bo Nguyen <nguyenbo2001@gmail.com>
2020-11-05 21:02:15 +0700

    method delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -48,6 +48,15 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
+
+    // [DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => {
+                res.redirect('back');
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController;
